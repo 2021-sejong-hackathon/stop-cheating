82793ab7048d329e5c766b12851ba5b38794fc57
devohda <fffvvv6@gmail.com>
2021-10-01 14:06:22 +0900

    https

--- webrtc-web-master/release/routes/index.js
+++ webrtc-web-master/release/routes/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 // 라우터 파일 가져오기
-router.get('/', )
+router.get('/hi', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
 
 // 라우터 파일 등록하기
 
